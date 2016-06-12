039137518ba2d7ba86d1f5f9427ee9f9424053df
Matt Greer <matt.e.greer@gmail.com>
2016-06-12 07:51:21 -0700

    basic webpack in place

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class App extends React.Component {
+    render() {
+        return (
+            <div>Simple React + Babel + Bootstrap + Webpack</div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector("#myApp"));
